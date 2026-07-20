979f89761fa5da4bf52ca16cd5579f09dc4c7c6d
Kuda_Valen <kudamukwasi@gmail.com>
2026-07-20 22:54:33 +0200

    working on system menu

--- waiting_list.py
+++ waiting_list.py
@@ -24,3 +24,37 @@ def ticket_setter():
     last_no = tickets[-1]
     ticket_no = last_no + 1
     return ticket_no
+
+if __name__ == "__main__":
+    customer = Customer()
+
+    while True:
+        print("\n== Ticket System ==\n")
+        print("1. Purchase a Ticket")
+        print("2. Print All Customers")
+        print("3. Print Artist Tickets Sold")
+        print("5. Exit")
+
+        try:
+            option = int(input("Choose an option: "))
+
+            if option == 1:
+                ...
+            
+            elif option == 2:
+                ...
+
+            elif option == 3:
+                ...
+            
+            elif option == 4:
+                ...
+            
+            elif option == 5:
+                print("\nExiting System...")
+            
+            else:
+                print("\nInvalid input!!.. Choose a valid optoin:")
+        
+        except ValueError as e:
+            print(f"\nEncountered an input error: {e}")
